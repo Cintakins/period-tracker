5667d8051feeac7d9fd0ed08447217eee1dc8f9a
Cintakins <jacintalerwill@hotmail.com>
2022-09-26 18:48:43 +0000

    began writing the sorting function. Still need to complete the ascending order part

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.db.models import Q
 
 # Create your views here.
-
+# used Boutique ado project to help inform this code
 def products(request):
 
     """
@@ -15,6 +15,7 @@ def products(request):
     products = Product.objects.all()
     search_query = None
     categories = None
+    direction = None
 
     if request.GET:
         if 'category' in request.GET:
@@ -33,10 +34,23 @@ def products(request):
                 ) | Q(name__icontains=search_query)
             products = products.filter(search_q)
 
+        
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            sort = request.GET['sort']
+            if direction == 'desc':
+                price_sort = f'-{sort}'
+            else:
+                price_sort = f'+{sort}'
+
+        products = products.order_by(price_sort)
+
+
     context = {
         'products': products,
         'searched_item': search_query,
         'product_categories': categories,
+        'direction': direction,
     }
 
     return render(request, 'products/products.html', context)
