e2333a83bee62b02980bd744cacdb517d3ab09ac
Cintakins <jacintalerwill@hotmail.com>
2022-09-27 17:43:59 +0000

    added product detail template, view and url path

--- products/views.py
+++ products/views.py
@@ -54,3 +54,18 @@ def products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+
+    """
+    Displays single product and full description
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
