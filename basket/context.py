9114b193d0c5e03f70db7972bca366c71759ed07
Cintakins <jacintalerwill@hotmail.com>
2022-09-28 08:56:53 +0000

    basket content and total price display are all working. Need to ad more styling to the template though

--- basket/context.py
+++ basket/context.py
@@ -1,11 +1,26 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
+
+# code informed by boutique-ado mini project
 def basket_contents(request):
 
     basket_items = []
     total = 0
     item_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        item_count += quantity 
+        total = quantity * product.price
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
 
