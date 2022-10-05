7f39152df67cb46beffab18133bab002026679e5
Cintakins <jacintalerwill@hotmail.com>
2022-10-05 10:31:37 +0000

    added html to checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from .forms import OrderForm
 
 
 def checkout(request):
@@ -10,9 +9,7 @@ def checkout(request):
         messages.error(request, 'There are no items in the basket')
         return redirect(reverse('products'))
     
-    order_form = OrderForm()
     context = {
-        'order_form': order_form,
     }
 
     return render(request, 'checkout/checkout.html', context)
