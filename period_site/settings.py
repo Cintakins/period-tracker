4d0a5ad7813198a432c2f867cb8086c28e57684d
Cintakins <jacintalerwill@hotmail.com>
2022-10-14 16:13:11 +0000

    added email vars to settings

--- period_site/settings.py
+++ period_site/settings.py
@@ -176,6 +176,12 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Wed, 31 Jan 2100 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'menstrual-tracker'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -207,6 +213,19 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET', '')
-DEFAULT_FROM_EMAIL = 'periodic@whatever.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'periodic@whatever.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
